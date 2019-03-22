d089e544df109bbe5d9ff6ad9382967bf8afd851
Adam Cozzette <acozzette@google.com>
2019-03-21 17:03:19 -0700

    Set up Kokoro to build JRuby gems

--- kokoro/release/ruby/linux/ruby/ruby_build.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build.sh
@@ -11,7 +11,14 @@ fi
 
 umask 0022
 pushd ruby
+
 bundle install && bundle exec rake gem:native
 ls pkg
 mv pkg/* $ARTIFACT_DIR
+
+# JRuby
+rvm use jruby
+rake build && gem build google-protobuf.gemspec
+mv google-protobuf-*-java.gem $ARTIFACT_DIR
+
 popd
